7aaef3d9260cb4e4a3e1d20f960a3033e0e24665
amperov <alex.amperov@yandex.ru>
2023-03-12 17:35:04 +0700

    upd

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -96,7 +96,7 @@ function Product() {
                             <Col span={8}>
                                 <Input style={{ marginBottom: 10 }} value={nameRu} onChange={e => setNameRu(e.target.value)}  placeholder="Введите наименование на русском" />
                                 <Input style={{ marginBottom: 10 }} value={nameEng} onChange={e => setNameEng(e.target.value)}  placeholder="Введите наименование на английском" />
-                                {/*<Input style={{marginBottom: 10}} value={digiid} onChange={e => setDigiid(e.target.value)}  placeholder="Введите айди для предпроверки" />*/}
+                                <Input style={{marginBottom: 10}} value={digiid} onChange={e => setDigiid(e.target.value)}  placeholder="Введите айди для предпроверки" />
                                 <Input style={{ marginBottom: 10 }} value={subtypeValue} onChange={e => setSubtypeValue(e.target.value)}  placeholder="Введите значение номинала" />
                                 {
                                     isComposite ?
@@ -175,9 +175,9 @@ function Product() {
                                             <p style={{marginTop:0}}>Название (EN): {isChangingName[1] & isChangingName[0] === i.id ?
                                                 <Input onClick={(e) => setNewNameEn(i.title_eng)} placeholder={i.title_eng} onChange={(e) => setNewNameEn(e.target.value)} value={newNameEn} style={{ width: 150, margin: 0, marginLeft: 60 }} /> : i.title_eng}
                                             </p>
-                                            {/*<p style={{marginTop:0}}>Айди для предпроверки: {isChangingName[1] & isChangingName[0] === i.id ?
+                                            <p style={{marginTop:0}}>Айди для предпроверки: {isChangingName[1] & isChangingName[0] === i.id ?
                                                 <Input  onClick={(e) => setNewSubitemId(i.subitem_id)} placeholder={i.subitem_id} onChange={(e) => setNewSubitemId(e.target.value)} value={newSubitemId} style={{ width: 150 }} /> : i.subitem_id}
-                                            </p>*/}
+                                            </p>
                                             <div id={"1"}>
                                                 <p style={{marginTop:0}}>Составные номиналы: {isChangingName[1] & isChangingName[0] === i.id ?
                                                     <Input onClick={(e) => setPartialValues(i.partial_values)} placeholder={i.subitem_id} onChange={(e) => setPartialValues(e.target.value)} value={partialValues} style={{ width: 150, marginLeft: 15 }} /> :
